69d9cfde52b5e0eebf8b1e09fc4b1cee45f33e7b
Nurudeen Adewale <35535871+Hardey18@users.noreply.github.com>
2023-09-21 09:01:58 +0100

    initial commit

---
+++ src/main/java/com/osiki/TBBank/TbBankApplication.java
@@ -0,0 +1,13 @@
+package com.osiki.TBBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osiki/TBBank/TbBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiki.TBBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
