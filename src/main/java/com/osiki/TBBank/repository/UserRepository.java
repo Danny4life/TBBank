273c0b2c4209042231568fa76d4077d5d498cdcf
Nurudeen Adewale <35535871+Hardey18@users.noreply.github.com>
2023-09-21 11:13:07 +0100

    create bank response, user request and account info dto

---
+++ src/main/java/com/osiki/TBBank/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.osiki.TBBank.repository;
+
+import com.osiki.TBBank.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
